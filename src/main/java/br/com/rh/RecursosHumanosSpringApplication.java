ef97a4f6b163144874310b0232d9f9e1fc6e8bb0
Anailson Ribeiro <arsantos315@gmail.com>
2020-05-09 11:20:55 -0300

    Projeto Criado

---
+++ src/main/java/br/com/rh/RecursosHumanosSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.rh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecursosHumanosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecursosHumanosSpringApplication.class, args);
+	}
+
+}
