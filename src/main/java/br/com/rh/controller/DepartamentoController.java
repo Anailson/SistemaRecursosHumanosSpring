783f8feaff22a6ab13491541fa14e8514caafa46
Anailson Ribeiro <arsantos315@gmail.com>
2020-06-06 16:37:44 -0300

    Finalizando Caso de Uso Departamento

--- src/main/java/br/com/rh/controller/DepartamentoController.java
+++ src/main/java/br/com/rh/controller/DepartamentoController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.rh.model.Departamento;
@@ -81,6 +83,18 @@ public class DepartamentoController {
 		modelAndView.addObject("departamentos", departamentoRepository.findAll());
 		modelAndView.addObject("departamentosobj", new Departamento());
 				
+		return modelAndView;
+	}
+	
+	
+	/*----PESQUISA DEPARTAMENTO------*/
+	@PostMapping("**/pesquisadepartamento")
+	public ModelAndView pesquisa(@RequestParam("nomepesquisa") String nomepesquisa) {
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastrodepartamento");
+		modelAndView.addObject("departamentos", departamentoRepository.findDepartamentoByName(nomepesquisa));
+		modelAndView.addObject("departamentosobj", new Departamento());
+		
 		return modelAndView;
 	}
 	
--- src/main/java/br/com/rh/repository/DepartamentoRepository.java
+++ src/main/java/br/com/rh/repository/DepartamentoRepository.java
@@ -1,5 +1,8 @@
 package br.com.rh.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,4 +13,9 @@ import br.com.rh.model.Departamento;
 @Transactional
 public interface DepartamentoRepository extends CrudRepository<Departamento, Long>{
 
+	
+	@Query("select p from Departamento p where p.nomedepartamento like %?1%")
+	List<Departamento> findDepartamentoByName(String nomedepartamento);
+	
+	
 }
