e8d912b9b1e0db4b74d6fa50b42af84f92c12436
Anailson Ribeiro <arsantos315@gmail.com>
2020-06-12 12:02:24 -0300

    Método de pesquisa por nome

--- src/main/java/br/com/rh/controller/CargoController.java
+++ src/main/java/br/com/rh/controller/CargoController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.rh.model.Cargo;
@@ -89,5 +91,16 @@ public class CargoController {
 		return modelAndView;
 	}	
 	
+	
+	/*----PESQUISA POR NOME DO CARGO------*/
+	@PostMapping("**/pesquisacargo")
+	public ModelAndView pesquisa(@RequestParam("nomepesquisa") String nomepesquisa) {
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastrocargo");
+		modelAndView.addObject("cargos", cargoRepository.findCargoByName(nomepesquisa));
+		modelAndView.addObject("cargosobj", new Cargo());
+		
+		return modelAndView;
+	}
 
 }
--- src/main/java/br/com/rh/repository/CargoRepository.java
+++ src/main/java/br/com/rh/repository/CargoRepository.java
@@ -1,14 +1,20 @@
 package br.com.rh.repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.rh.model.Cargo;
 
+
 @Transactional
 @Repository
 public interface CargoRepository extends CrudRepository<Cargo, Long>{
 
+	@Query("select p from Cargo p where p.nomecargo like %?1%")
+	List<Cargo> findCargoByName(String nomecargo);
 	
 }
