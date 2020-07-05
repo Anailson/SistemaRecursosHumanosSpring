7eb83dc053bd9a2e767f510ea0ef724df0b18023
Anailson Ribeiro <arsantos315@gmail.com>
2020-07-04 21:27:26 -0300

    Criando a classe ReportUtil

---
+++ src/main/java/br/com/rh/controller/ReportUtil.java
@@ -0,0 +1,48 @@
+package br.com.rh.controller;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.stereotype.Component;
+
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+/*CLASSE GENERICA PARA QQL RELATORIO*/
+@Component
+public class ReportUtil implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	/*RETORNA O NOSSO PDF EM BYTE PARA DOWLOAD NO NAVEGADOR*/
+	public byte [] gerarRelatorio (List listDados, 
+			String relatorio, ServletContext servletContext)
+	
+	throws Exception{
+		
+		/*CRIA A LISTA DE DADOS PARA O RELATORIO COM NOSSA LISTA DE OBJETOS PARA IMPRIMIR*/
+		JRBeanCollectionDataSource jrdcds = new JRBeanCollectionDataSource(listDados);
+				
+		/*CARREGA O CAMINHO DO ARQUIVO JASPER COMPILADO*/
+		String caminhoJasper = servletContext.getRealPath("relatorio")
+				+ File.separator + relatorio + ".jasper";
+		
+		/*CARREGA O ARQUIBO JASPER PASSANDO OS DADOS*/
+		JasperPrint impressoraJasper = JasperFillManager
+				.fillReport(caminhoJasper, new HashMap(), jrdcds);
+		
+		/*EXPORTA PARA BYTE[] PARA FAZER O DOWLOAD DO PDF OU XLS POR EXEMPLO*/	
+		return JasperExportManager.exportReportToPdf(impressoraJasper);
+		
+		
+		
+	}
+	
+	
+}
